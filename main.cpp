f2cb89d472d1aa0e9caf62648bcef8e6ac7d4617
drWindstrom <jan.winstroth@outlook.com>
2021-09-17 16:29:33 +0200

    Quick save

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include "MainWindow.h"
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    QWidget w;
+    w.setWindowTitle("Todo App without Designer");
+
+    QPushButton *addTaskButton = new QPushButton(&w);
+    addTaskButton->setText("Add Task");
+
+    addTaskButton->setGeometry(50, 50, 100, 40);
+
+    QLabel *taskLabel = new QLabel(&w);
+    taskLabel->setText("Tasks");
+    taskLabel->setGeometry(0, 50, 100, 20);
+
+
     w.show();
     return a.exec();
 }
